ad6a6c6e08e08d7c6287b13eba373af1a31a9e4f
mikodham <dhammikoarya@gmail.com>
2021-11-23 23:21:29 +0900

    clone soccernet

--- Dataset/download_SoccerNet.py
+++ Dataset/download_SoccerNet.py
@@ -3,6 +3,7 @@ from SoccerNet.Downloader import SoccerNetDownloader
 
 mySoccerNetDownloader = SoccerNetDownloader(LocalDirectory="SoccerNet")
 
+# mySoccerNetDownloader.downloadGames(files=["results_spotting.json"], split=["train","valid","test"]) # download HQ Videos
 # # Download SoccerNet labels
 # mySoccerNetDownloader.downloadGames(files=["Labels.json"], split=["train","valid","test"]) # download labels
 # mySoccerNetDownloader.downloadGames(files=["Labels-v2.json"], split=["train","valid","test"]) # download labels SN v2
@@ -10,7 +11,7 @@ mySoccerNetDownloader = SoccerNetDownloader(LocalDirectory="SoccerNet")
 
 
 # Download SoccerNet features
-# mySoccerNetDownloader.downloadGames(files=["1_ResNET_TF2.npy", "2_ResNET_TF2.npy"], split=["train","valid","test"]) # download Features
+mySoccerNetDownloader.downloadGames(files=["1_ResNET_TF2.npy", "2_ResNET_TF2.npy"], split=["train","valid","test"]) # download Features
 # mySoccerNetDownloader.downloadGames(files=["1_ResNET_TF2_PCA512.npy", "2_ResNET_TF2_PCA512.npy"], split=["train","valid","test"]) # download Features reduced with PCA
 # mySoccerNetDownloader.downloadGames(files=["1_player_boundingbox_maskrcnn.json", "2_player_boundingbox_maskrcnn.json"], split=["train","valid","test"]) # download Player Bounding Boxes inferred with MaskRCNN
 # mySoccerNetDownloader.downloadGames(files=["1_field_calib_ccbv.json", "2_field_calib_ccbv.json"], split=["train","valid","test"]) # download Field Calibration inferred with CCBV
@@ -18,9 +19,9 @@ mySoccerNetDownloader = SoccerNetDownloader(LocalDirectory="SoccerNet")
 
 
 # Download SoccerNet videos (require password from NDA to download videos)
-mySoccerNetDownloader.password = input("Password for videos? (contact the author):\n")
+# mySoccerNetDownloader.password = input("Password for videos? (contact the author):\n")
 # mySoccerNetDownloader.downloadGames(files=["1.mkv", "2.mkv"], split=["train","valid","test"]) # download LQ Videos
-mySoccerNetDownloader.downloadGames(files=["1_HQ.mkv", "2_HQ.mkv", "video.ini"], split=["train","valid","test"]) # download HQ Videos
+# mySoccerNetDownloader.downloadGames(files=["1_HQ.mkv", "2_HQ.mkv", "video.ini"], split=["train","valid","test"]) # download HQ Videos
 
 
 # Download SoccerNet Challenge set (require password from NDA to download videos)
